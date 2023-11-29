f28a9be0ca81b0c4b29e4650cee6c6e52f8aadd8
Vinicius de Souza <vdsbt33@gmail.com>
2023-11-28 22:17:46 -0300

    Added CORS with config whitelisting localhost:8080 and addPerson.

--- src/main.go
+++ src/main.go
@@ -4,8 +4,10 @@ import (
 	"database/sql"
 	"net/http"
 	"time"
+    "fmt"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 	_ "github.com/lib/pq"
 )
 
@@ -18,38 +20,63 @@ type Person struct
     UpdatedAt sql.NullTime `json:"updatedAt"`
 }
 
-func getPeople(c *gin.Context) {
-    result := []Person 	{
-        {
-            Id: "3ba86131-36cf-4052-b312-11ddbaa8fe9f",
-            Name: "One",
-            Email: "one@email.com",
-            CreatedAt: time.Now(),
-            UpdatedAt: sql.NullTime {
-                Time: time.Now(),
-                Valid: true,
-            },
+var personList = []Person 	{
+    {
+        Id: "3ba86131-36cf-4052-b312-11ddbaa8fe9f",
+        Name: "One",
+        Email: "one@email.com",
+        CreatedAt: time.Now(),
+        UpdatedAt: sql.NullTime {
+            Time: time.Now(),
+            Valid: true,
         },
-        {
-            Id: "6dca7cd3-87a9-497d-8cbc-4250872c65a0",
-            Name: "Two",
-            Email: "two@email.com",
-            CreatedAt: time.Now(),
-            UpdatedAt: sql.NullTime {},
-        },
+    },
+    {
+        Id: "6dca7cd3-87a9-497d-8cbc-4250872c65a0",
+        Name: "Two",
+        Email: "two@email.com",
+        CreatedAt: time.Now(),
+        UpdatedAt: sql.NullTime {},
+    },
+}
 
+func getPeople(c *gin.Context) {
+    c.JSON(http.StatusOK, personList)
+}
+
+func addPerson(c *gin.Context) {
+    var requestPerson Person
+
+    if err := c.BindJSON(&requestPerson); err != nil {
+        fmt.Print(err)
+        c.JSON(http.StatusBadRequest, err)
+        return
     }
 
-    c.JSON(http.StatusOK, result)
+    personList = append(personList, requestPerson)
+    c.JSON(http.StatusCreated, nil)
 }
 
 func createRoutes(router *gin.Engine) {
 	router.GET("/people", getPeople)
+	router.POST("/people", addPerson)
 }
 
 func main() {
 	router := gin.Default()
+    router.Use(gin.Logger())
+    router.Use(gin.Recovery())
+
+    config := cors.DefaultConfig()
+    config.AllowOrigins = []string{ "http://localhost:8080", "http://127.0.0.1:8080" }
+    config.AllowMethods = []string{ "GET", "POST", "PUT", "DELETE" }
+    router.Use(cors.New(config))
+
 	createRoutes(router)
 
-	router.Run("localhost:8080")
+    err := router.Run("localhost:8082")
+    
+    if err != nil {
+        fmt.Printf("An error occurred: %+v\n", err)
+    }
 }
